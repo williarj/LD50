98d7acc3e8f2635d3cd7fa6de72f3fc5743f7250
jagapher <48227538+jagapher@users.noreply.github.com>
2022-04-02 00:16:50 -0400

    forgot to add the script file

---
+++ Project/scripts/Globals.gd
@@ -0,0 +1,14 @@
+class_name Globals
+extends Reference
+
+enum Cardinal {
+	NORTH = 0,
+	EAST  = 1,
+	SOUTH = 2,
+	WEST  = 4
+}
+
+static func opposite_direction(cardinal : int) -> int:
+	assert(cardinal in Cardinal.values(), "wrong cardinal value")
+	
+	return (cardinal + 2) % 4
